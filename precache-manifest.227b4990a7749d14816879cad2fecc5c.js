250e6895d308634156663888ac6d29c8e64a8ea1
buttiprathyu <pbutti@email.arizona.edu>
2020-05-31 16:43:01 -0700

    Updates

---
+++ precache-manifest.227b4990a7749d14816879cad2fecc5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4994e3f80eb7230b97945425ee5a6a40",
+    "url": "/myFirstReactProj/index.html"
+  },
+  {
+    "revision": "8c49a939fdfcfa895846",
+    "url": "/myFirstReactProj/static/css/main.b5165da5.chunk.css"
+  },
+  {
+    "revision": "12240f73676d8f2fbd85",
+    "url": "/myFirstReactProj/static/js/2.a3c82928.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/myFirstReactProj/static/js/2.a3c82928.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c49a939fdfcfa895846",
+    "url": "/myFirstReactProj/static/js/main.4ddaf7d6.chunk.js"
+  },
+  {
+    "revision": "566ed1452afc88f3a1d8",
+    "url": "/myFirstReactProj/static/js/runtime-main.06735bc7.js"
+  }
+]);
